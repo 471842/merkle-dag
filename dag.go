a0066fb848a905e49adb61ab174c0c9f7a9eb956
zzy <2721775682@qq.com>
2024-03-31 16:36:19 +0800

    1

--- dag.go
+++ dag.go
@@ -5,6 +5,11 @@ import (
 	"hash"
 )
 
+const (
+	K          = 1 << 10
+	BLOCK_SIZE = 256 * K
+)
+
 type Link struct {
 	Name string
 	Hash []byte
@@ -17,35 +22,75 @@ type Object struct {
 }
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// 如果是文件,直接存储文件内容
-	if node.Type() == FILE {
-		file, _ := node.(File)
-		data := file.Bytes()
-		h.Write(data)
-		store.Put(h.Sum(nil), data)
-		return h.Sum(nil)
+	switch node.Type() {
+	case FILE:
+		return StoreFile(store, node.(File), h)
+	case DIR:
+		return StoreDir(store, node.(Dir), h)
+	}
+	return nil
+}
+
+func StoreFile(store KVStore, file File, h hash.Hash) []byte {
+	t := []byte("blob")
+	if file.Size() > BLOCK_SIZE {
+		t = []byte("list")
 	}
 
-	// 如果是目录,则递归存储子节点
-	dir, _ := node.(Dir)
-	var links []Link
-	for it := dir.It(); it.Next(); {
+	data := file.Bytes()
+	if len(data) <= BLOCK_SIZE {
+		// 文件小于等于BLOCK_SIZE，直接存储
+		h.Reset()
+		h.Write(t)
+		h.Write(data)
+		hash := h.Sum(nil)
+		store.Put(hash, data)
+		return hash
+	} else {
+		// 文件大于BLOCK_SIZE，分块存储
+		var links []Link
+		for i := 0; i < len(data); i += BLOCK_SIZE {
+			end := i + BLOCK_SIZE
+			if end > len(data) {
+				end = len(data)
+			}
+			block := data[i:end]
+			h.Reset()
+			h.Write([]byte("blob"))
+			h.Write(block)
+			hash := h.Sum(nil)
+			store.Put(hash, block)
+			links = append(links, Link{Hash: hash, Size: len(block)})
+		}
+		// 存储分块链接
+		linksData, _ := json.Marshal(links)
+		h.Reset()
+		h.Write(t)
+		h.Write(linksData)
+		hash := h.Sum(nil)
+		store.Put(hash, linksData)
+		return hash
+	}
+}
+
+func StoreDir(store KVStore, dir Dir, h hash.Hash) []byte {
+	var tree Object
+	tree.Links = make([]Link, 0)
+	tree.Data = make([]byte, 0)
+
+	it := dir.It()
+	for it.Next() {
 		node := it.Node()
 		hash := Add(store, node, h)
-		size := node.Size()
-		links = append(links, Link{
-			Name: node.Name(),
-			Hash: hash,
-			Size: int(size),
-		})
+		link := Link{Name: node.Name(), Hash: hash, Size: int(node.Size())}
+		tree.Links = append(tree.Links, link)
 	}
 
-	obj := Object{
-		Links: links,
-	}
-	data, _ := json.Marshal(obj)
-	h.Write(data)
-	objectHash := h.Sum(nil)
-	store.Put(objectHash, data)
-	return objectHash
+	treeData, _ := json.Marshal(tree)
+	h.Reset()
+	h.Write([]byte("tree"))
+	h.Write(treeData)
+	hash := h.Sum(nil)
+	store.Put(hash, treeData)
+	return hash
 }
